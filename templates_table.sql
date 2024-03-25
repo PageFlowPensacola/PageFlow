be51f4e18c42e9181bc9d452735ea70a2dc8ecc6
mikeill <mike@mzoo.org>
2024-03-25 09:38:43 -0500

    Add two new tables.

--- templates_table.sql
+++ templates_table.sql
@@ -12,25 +12,20 @@ CREATE TABLE template_pages (
 	page_data BYTEA NOT NULL
 );
 
--- TODO following two
-CREATE TABLE `template_signatory` (
-  `template_signatory_id` bigint(15) NOT NULL,
-  `name` varchar(256) NOT NULL,
-  `page_group_id` bigint(15) DEFAULT NULL,
-  PRIMARY KEY (`template_signatory_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE template_signatories (
+	id BIGSERIAL PRIMARY KEY,
+	name varchar NOT NULL,
+	template_id int NOT NULL REFERENCES templates
+);
 
-CREATE TABLE `audit_rect` (
-  `audit_rect_id` bigint(15) NOT NULL,
-  `audit_type` varchar(45) NOT NULL,
-  `x1` smallint(6) NOT NULL,
-  `y1` smallint(6) NOT NULL,
-  `x2` smallint(6) NOT NULL,
-  `y2` smallint(6) NOT NULL,
-  `page_type_id` bigint(15) NOT NULL,
-  `name` varchar(256) DEFAULT NULL,
-  `template_signatory_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`audit_rect_id`),
-  KEY `fk_audit_rect_page_type1_idx` (`page_type_id`),
-  CONSTRAINT `fk_audit_rect_page_type1` FOREIGN KEY (`page_type_id`) REFERENCES `page_type` (`page_type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE audit_rects (
+  id BIGSERIAL PRIMARY KEY,
+  audit_type varchar NOT NULL, -- initials, signature, date
+  x1 smallint NOT NULL,
+  y1 smallint NOT NULL,
+  x2 smallint NOT NULL,
+  y2 smallint NOT NULL,
+	template_id int NOT NULL REFERENCES templates,
+  name varchar DEFAULT NULL,
+  template_signatory_id int NOT NULL REFERENCES template_signatories
+);
