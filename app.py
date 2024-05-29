ef1e2589c7ddcde5a82666e7e8112cdf4b2c8dfa
mikeill <mike@mzoo.org>
2024-05-29 11:46:38 -0500

    Incorporate python app with River ml library into this codebase.

---
+++ app.py
@@ -0,0 +1,41 @@
+from river import compose
+from river import feature_extraction
+from river import naive_bayes
+import os
+import sys
+import collections
+import pprint
+import pickle
+import time
+import json
+
+start = time.monotonic()
+
+try:
+    with open("model.dat", "rb") as m: model = pickle.load(m)
+except FileNotFoundError:
+    model = compose.Pipeline(
+        ("tokenize", feature_extraction.BagOfWords(lowercase=False, ngram_range=(1, 2))),
+        ("nb", naive_bayes.ComplementNB(alpha=1))
+    )
+
+""" input = {
+    "pageref": "42:3",
+    "text": "When I was a lad I served a term As",
+    "msgid": "1",
+} """
+
+try:
+    print("Ready", file=sys.stderr, flush=True)
+    while 1:
+        print("Waiting", file=sys.stderr, flush=True)
+        msg = json.loads(input()) # input looks for one line of txt followed by newline
+        if ("pageref" in msg):
+            model.learn_one(msg["text"], msg["pageref"])
+            with open("model.dat", "wb") as m:
+                pickle.dump(model, m)
+            print(json.dumps({"status": "ok", "msgid": msg["msgid"]}))
+        else:
+            res = model.predict_proba_one(msg["text"])
+            print(json.dumps({"result": res, "msgid": msg["msgid"]}))
+except EOFError: pass
--- utils.pike
+++ utils.pike
@@ -91,7 +91,7 @@ __async__ void tesseract(){
 }
 
 int ml() {
-	object model = G->bootstrap("modules/model.pike");
+	object model = G->bootstrap("modules/classifier.pike");
 	model.send_msg(([
 		"text": "Pirates",
 	]));
